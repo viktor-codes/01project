d8f54ebd0f72966b0b4657c368a95621ba7274ce
Viktor Rura <ruravictor@gmail.com>
2024-06-09 13:05:37 +0100

    Added handling errors for fetching items

--- src/App.js
+++ src/App.js
@@ -7,15 +7,30 @@ import SearchItem from "./SearchItem";
 import { useState, useEffect } from "react";
 
 function App() {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppingList")) || []);
+    const API_URL = "http://localhost:3500/itemss";
 
+    const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState("");
-
     const [search, setSearch] = useState("");
+    const [fetchError, setFetchError] = useState(null);
 
     useEffect(() => {
-        localStorage.setItem("shoppingList", JSON.stringify(items));
-    }, [items]);
+        const fetchItems = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if (!response.ok) throw new Error("Failed to fetch items");
+
+                const listItems = await response.json();
+                console.log(listItems);
+                setItems(listItems);
+                setFetchError(null);
+            } catch (error) {
+                setFetchError(error.message);
+            }
+        };
+
+        (async () => await fetchItems())();
+    }, []);
 
     const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -56,13 +71,16 @@ function App() {
                 handleSubmit={handleSubmit}
             />
             <SearchItem search={search} setSearch={setSearch} />
-            <Content
-                items={items.filter((item) =>
-                    item.item.toLowerCase().includes(search.toLowerCase())
-                )}
-                handleCheck={handleCheck}
-                handleDelete={handleDelete}
-            />
+            <main>
+                {fetchError && <p style={{ color: "red" }}>{fetchError}</p>}
+                <Content
+                    items={items.filter((item) =>
+                        item.item.toLowerCase().includes(search.toLowerCase())
+                    )}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            </main>
             <Footer length={items.length} />
         </div>
     );
--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,19 @@
-import ItemList from './ItemList';
+import ItemList from "./ItemList";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
-  return (
-    <main>
-      {items.length ? (
-        <ItemList 
-        items={items}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-        />
-      ) : (
-        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
-      )}
-    </main>
-  );
+    return (
+        <>
+            {items.length ? (
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+            )}
+        </>
+    );
 };
 
-export default Content
+export default Content;
