c8baf3addc8b8b3c3933f8ca5d32f81730c9c88b
Viktor Rura <ruravictor@gmail.com>
2024-06-09 15:14:12 +0100

    Implemented the CRUD operations for the grocery list items

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Content from "./Content";
 import Footer from "./Footer";
 import SearchItem from "./SearchItem";
 import { useState, useEffect } from "react";
+import apiRequest from "./apiRequest";
 
 function App() {
-    const API_URL = "http://localhost:3500/itemss";
+    const API_URL = "http://localhost:3500/items";
 
     const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState("");
     const [search, setSearch] = useState("");
     const [fetchError, setFetchError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchItems = async () => {
@@ -21,18 +23,21 @@ function App() {
                 if (!response.ok) throw new Error("Failed to fetch items");
 
                 const listItems = await response.json();
-                console.log(listItems);
+
                 setItems(listItems);
                 setFetchError(null);
             } catch (error) {
                 setFetchError(error.message);
+            } finally {
+                setLoading(false);
             }
         };
-
-        (async () => await fetchItems())();
+        setTimeout(() => {
+            (async () => await fetchItems())();
+        }, 500);
     }, []);
 
-    const addItem = (item) => {
+    const addItem = async (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = {
             id,
@@ -41,18 +46,45 @@ function App() {
         };
         const listItems = [...items, myNewItem];
         setItems(listItems);
+
+        const postOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(myNewItem),
+        };
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) setFetchError(result);
     };
 
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
         const listItems = items.map((item) =>
             item.id === id ? { ...item, checked: !item.checked } : item
         );
         setItems(listItems);
+
+        const myItem = listItems.filter((item) => item.id === id);
+        const updatOptions = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({checked: myItem[0].checked}),
+        };
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, updatOptions);
+        if (result) setFetchError(result);
     };
 
-    const handleDelete = (id) => {
+    const handleDelete = async (id) => {
         const listItems = items.filter((item) => item.id !== id);
         setItems(listItems);
+
+        const deleteOptions = {method: "DELETE"};
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, deleteOptions);
+        if (result) setFetchError(result);
     };
 
     const handleSubmit = (e) => {
@@ -72,14 +104,19 @@ function App() {
             />
             <SearchItem search={search} setSearch={setSearch} />
             <main>
+                {loading && <p>Loading...</p>}
                 {fetchError && <p style={{ color: "red" }}>{fetchError}</p>}
-                <Content
-                    items={items.filter((item) =>
-                        item.item.toLowerCase().includes(search.toLowerCase())
-                    )}
-                    handleCheck={handleCheck}
-                    handleDelete={handleDelete}
-                />
+                {!fetchError && !loading && (
+                    <Content
+                        items={items.filter((item) =>
+                            item.item
+                                .toLowerCase()
+                                .includes(search.toLowerCase())
+                        )}
+                        handleCheck={handleCheck}
+                        handleDelete={handleDelete}
+                    />
+                )}
             </main>
             <Footer length={items.length} />
         </div>
